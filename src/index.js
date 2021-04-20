c77fceff2a0b2bbe2d1237fdb908b1184bc7c9b0
Akshay Kumar <akshay.nm92@gmail.com>
2021-04-20 11:56:58 +0530

    switch to object export

--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
 import { useCallback, useEffect, useState } from 'react'
+function camelize(str) {
+  return str
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase()
+    })
+    .replace(/\s+/g, '')
+}
+const generateReturnPayload = (
+  names = [],
+  values = [],
+  warnings = [],
+  onChangeHandlers = []
+) => {
+  const payload = {}
+  if (
+    names.length === values.length &&
+    values.length === warnings.length &&
+    warnings.length === onChangeHandlers.length
+  ) {
+    names.forEach((name, i) => {
+      payload[camelize(name)] = values[i]
+      payload[camelize(`on ${name} Change`)] = onChangeHandlers[i]
+      payload[camelize(`show ${name} Warning`)] = warnings[i]
+    })
+  } else {
+    names.forEach((name) => {
+      payload[camelize(name)] = ''
+      payload[camelize(`on ${name} Change`)] = () => {}
+      payload[camelize(`show ${name} Warning`)] = false
+    })
+  }
+  return payload
+}
 
-const useFormState = (spec, debug = false) => {
-  const [names] = useState(spec.states.map((state) => state.name))
-  const [values, setValues] = useState(
-    spec.states.map((state) => state.default)
-  )
-  const [mandatory] = useState(spec.states.map((state) => state.mustBeValid))
-  const [validators] = useState(spec.states.map((state) => state.validator))
-  const [valids, setValids] = useState(
-    spec.states.map((state) => state.isValid)
-  )
+const useFormState = ({ states, debug }) => {
+  const [names] = useState(states.map((state) => state.name))
+  const [values, setValues] = useState(states.map((state) => state.default))
+  const [mandatory] = useState(states.map((state) => state.mustBeValid))
+  const [validators] = useState(states.map((state) => state.validator))
+  const [valids, setValids] = useState(states.map((state) => state.isValid))
   const [needValidations, setNeedValidations] = useState(
-    spec.states.map(() => false)
+    states.map(() => false)
   )
-  const [edits, setEdits] = useState(spec.states.map(() => false))
-  const [warnings, setWarnings] = useState(spec.states.map(() => false))
+  const [edits, setEdits] = useState(states.map(() => false))
+  const [warnings, setWarnings] = useState(states.map(() => false))
   const [isValid, setIsValid] = useState(false)
   const [isValidating, setIsValidating] = useState(false)
 
@@ -105,22 +134,22 @@ const useFormState = (spec, debug = false) => {
   }, [])
 
   const reset = useCallback(() => {
-    setValues(spec.states.map((state) => state.default))
-    setValids(spec.states.map((state) => state.isValid))
-    setNeedValidations(spec.states.map(() => false))
+    setValues(states.map((state) => state.default))
+    setValids(states.map((state) => state.isValid))
+    setNeedValidations(states.map(() => false))
     setIsValid(false)
-  }, [spec])
+  }, [states])
 
   const resetState = useCallback(
     (index) => {
       setValues((prev) => {
         const arr = [...prev]
-        arr[index] = spec.states[index].default
+        arr[index] = states[index].default
         return arr
       })
       setValids((prev) => {
         const arr = [...prev]
-        arr[index] = spec.states[index].defaultIsValid
+        arr[index] = states[index].defaultIsValid
         return arr
       })
       setNeedValidations((prev) => {
@@ -129,7 +158,7 @@ const useFormState = (spec, debug = false) => {
         return arr
       })
     },
-    [spec]
+    [states]
   )
 
   const setState = useCallback((index, value, isValid, needsValidation) => {
@@ -151,13 +180,13 @@ const useFormState = (spec, debug = false) => {
   }, [])
 
   const [onChangeHandlers] = useState(
-    spec.states.map((state, index) => (value) => onChangeHandler(index, value))
+    states.map((state, index) => (value) => onChangeHandler(index, value))
   )
   const [resetStates] = useState(
-    spec.states.map((state, index) => () => resetState(index))
+    states.map((state, index) => () => resetState(index))
   )
   const [setStates] = useState(
-    spec.states.map((state, index) => (value, isValid, needsValidation) =>
+    states.map((state, index) => (value, isValid, needsValidation) =>
       setState(index, value, isValid, needsValidation)
     )
   )
@@ -197,19 +226,24 @@ const useFormState = (spec, debug = false) => {
     if (debug) console.log('form state: onChangeHandlers: ', onChangeHandlers)
   }, [debug, onChangeHandlers])
 
-  return [
+  const [payload, setPayload] = useState(
+    generateReturnPayload(names, values, warnings, onChangeHandlers)
+  )
+
+  useEffect(() => {
+    setPayload(generateReturnPayload(names, values, warnings, onChangeHandlers))
+  }, [names, values, warnings, onChangeHandlers])
+
+  return {
     isValid,
     isValidating,
-    values,
-    warnings,
-    onChangeHandlers,
     triggerWarnings,
     triggerValidation,
     reset,
     resetStates,
     setStates,
-    names
-  ]
+    ...payload
+  }
 }
 
 export default useFormState
